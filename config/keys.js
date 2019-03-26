bd440fcc1104b08acbb2bcfac281182748fb2c3c
Kent Yip <kentasaurus@gmail.com>
2019-03-26 01:38:44 -0600

    Added Jwt Token code

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://kent:kent@devconnector-bpsn3.mongodb.net/test?retryWrites=true"
+    "mongodb+srv://kent:kent@devconnector-bpsn3.mongodb.net/test?retryWrites=true",
+  secretOrKey: "secret"
 };
